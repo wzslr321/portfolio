4356fec7d4fea680d8b03704d06cd6b34060ad38
Wiktor ZAając <wiktor.zajac888@gmail.com>
2021-02-21 14:29:59 +0100

    conflict fix

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	let name: string = 'world';
+	export let name: string;
 </script>
 
 <main>
@@ -13,14 +13,12 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-
 		h1 {
 			color: #ff3e00;
 			text-transform: uppercase;
 			font-size: 4em;
 			font-weight: 100;
 		}
-
 		@media (min-width: 640px) {
 			max-width: none;
 		}
