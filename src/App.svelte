7af35d4a88e9ddb819de292cbeb1be0ac39f333e
Wiktor ZAając <wiktor.zajac888@gmail.com>
2021-02-21 16:14:48 +0100

    header component

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,19 @@
 <script lang="ts">
-	export let name: string;
+    import Header from './Header.svelte';
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <Header/>
 </main>
 
 <style lang="scss">
+  	:global(body){
+		background-color:#101820FF;
+	}
 	main {
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
 		margin: 0 auto;
-		h1 {
-			color: #ff3e00;
-			text-transform: uppercase;
-			font-size: 4em;
-			font-weight: 100;
-		}
 		@media (min-width: 640px) {
 			max-width: none;
 		}
---
+++ src/Header.svelte
@@ -0,0 +1,31 @@
+<header>
+
+    <h1> Wiktor Zając  </h1>
+
+    <h2> Programming Enthusiast who loves to learn new things.  </h2>
+
+</header>
+
+<svelte:head>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+</svelte:head>
+
+<style lang="scss">
+    header{
+
+      text-transform: uppercase;
+      letter-spacing:2px;
+      font-family: 'Roboto', sans-serif;
+
+      h1 {
+        color: #b2b2b2;
+        font-size: 3em;
+      }
+
+      h2{
+        color: #F2AA4CFF;
+      }
+
+    }
+</style>
